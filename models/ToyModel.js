bd5ed0befb14db83f89bed06449979f4670f2e13
Ducanh3012 <116421180+Ducanh3012@users.noreply.github.com>
2023-12-23 07:29:44 +0700

    update

---
+++ models/ToyModel.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var ToySchema = mongoose.Schema({
+   name: String,
+   description: String,
+   price: Number,
+   brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'brands'  
+   }
+});
+
+var ToyModel = mongoose.model('toys', ToySchema);
+module.exports = ToyModel;
